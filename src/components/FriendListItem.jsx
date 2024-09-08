280f8e769589c615859bfb2832b60219df37c43a
Antonina Sych <antoninasych@gmail.com>
2024-09-08 20:28:30 +0100

    temp

--- src/components/FriendListItem.jsx
+++ src/components/FriendListItem.jsx
@@ -3,9 +3,11 @@ import styles from './css/FriendListItem.module.css';
 const FriendListItem = ({ avatar, name, isOnline }) => {
     return (
         <div className={styles.item}>
-            <span className={isOnline ? styles.online : styles.offline}></span>
-            <img className={styles.avatar} src={avatar} alt={name} width="48" />
+            <img className={styles.avatar} src={avatar} alt={`${name}'s avatar`} />
             <p className={styles.name}>{name}</p>
+            <p className={isOnline ? styles.online : styles.offline}>
+                {isOnline ? 'Online' : 'Offline'}
+            </p>
         </div>
     );
 };
